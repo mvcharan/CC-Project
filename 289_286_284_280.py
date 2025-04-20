846533d95362d4d314e08f6d9bb78d4d45796971
mvcharan <venkatcharanm@gmail.com>
2025-04-20 16:17:56 +0530

    Week 3 implemented

--- 289_286_284_280.py
+++ 289_286_284_280.py
@@ -10,7 +10,6 @@ client = docker.from_env()
 nodes = {}  # {id: {container_id, cpu_cores, status, pods: []}}
 pods = {}   # {id: {node_id, cpu_request}}
 
-
 def heartbeat_monitor():
     while True:
         for node_id in list(nodes.keys()):
@@ -20,6 +19,20 @@ def heartbeat_monitor():
                 nodes[node_id]['status'] = container.status
             except Exception:
                 nodes[node_id]['status'] = "unreachable"
+
+                # Failure Recovery 
+                print(f"[Recovery] Node {node_id} is unreachable. Attempting to migrate its pods...")
+
+                for pod_id in nodes[node_id]['pods'][:]:  
+                    pod = pods[pod_id]
+                    new_node = schedule_pod(pod['cpu_request'])
+                    if new_node:
+                        pods[pod_id]['node_id'] = new_node
+                        nodes[new_node]['pods'].append(pod_id)
+                        nodes[node_id]['pods'].remove(pod_id)
+                        print(f"[Recovery] Pod {pod_id} migrated to Node {new_node}")
+                    else:
+                        print(f"[Recovery] No available node to migrate pod {pod_id}")
         time.sleep(10)
 
 threading.Thread(target=heartbeat_monitor, daemon=True).start()
@@ -49,19 +62,8 @@ def add_node():
 def list_nodes():
     return jsonify(nodes)
 
-# @app.route('/stop_node/<node_id>', methods=['DELETE'])
-# def stop_node(node_id):
-#     if node_id not in nodes:
-#         return jsonify({"error": "Node not found"}), 404
-#     try:
-#         container = client.containers.get(nodes[node_id]['container_id'])
-#         container.stop()
-#         container.remove()
-#         del nodes[node_id]
-#         return jsonify({"message": "Node removed", "node_id": node_id})
-#     except Exception as e:
-#         return jsonify({"error": str(e)}), 500
 
+@app.route('/stop_node/<node_id>', methods=['DELETE'])
 @app.route('/stop_node/<node_id>', methods=['DELETE'])
 def stop_node(node_id):
     if node_id not in nodes:
@@ -70,16 +72,32 @@ def stop_node(node_id):
         container = client.containers.get(nodes[node_id]['container_id'])
         container.stop()
         container.remove()
+        orphaned_pods = [pod_id for pod_id in nodes[node_id]['pods']]
+        migrated_pods = []
+        failed_pods = []
 
-        # Remove all associated pods
-        orphaned_pods = [pod_id for pod_id, pod in pods.items() if pod['node_id'] == node_id]
         for pod_id in orphaned_pods:
-            del pods[pod_id]
+            pod = pods[pod_id]
+            new_node = schedule_pod(pod['cpu_request'])
+            if new_node:
+                pods[pod_id]['node_id'] = new_node
+                nodes[new_node]['pods'].append(pod_id)
+                migrated_pods.append(pod_id)
+            else:
+                failed_pods.append(pod_id)
 
         del nodes[node_id]
-        return jsonify({"message": "Node and its pods removed", "node_id": node_id, "removed_pods": orphaned_pods})
+
+        return jsonify({
+            "message": "Node and its pods removed",
+            "node_id": node_id,
+            "removed_pods": orphaned_pods,
+            "migrated_pods": migrated_pods,
+            "failed_pods": failed_pods
+        })
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
 
 @app.route('/launch_pod', methods=['POST'])
 def launch_pod():
